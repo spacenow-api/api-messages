92323fc606d7334002bd10da6a0cb69bb600f3f7
Arthemus <arthemus.moreira@gmail.com>
2019-10-24 12:15:17 +1100

    Fixing database config.

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   development: {
-    username: "spacenowtest" || process.env.DATABASE_USERNAME,
-    password: "Spac.918273!" || process.env.DATABASE_PASSWORD,
-    database: "spacenow_2019" || process.env.DATABASE_DATABASE,
-    host: "spacenow-test.cjo4zy3wnflc.ap-southeast-2.rds.amazonaws.com" || process.env.DATABASE_HOST,
-    dialect: "mysql",
-    logging: true,
+    dialect: 'mysql',
+    database: process.env.DATABASE_SCHEMA,
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    logging: process.env.DEBUG ? console.debug : false,
     underscored: true,
     freezeTableName: true
   }
